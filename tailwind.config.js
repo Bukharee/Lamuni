25f0ea5facfa341dd98d8b30cc27e7b7b515088c
bbkrmuhd <bbkrmuhdsaddiq@gmail.com>
2022-07-14 18:31:29 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    colors: {
+      'light-orange': '#fbf8f3',
+      'white': '#ffffff',
+      'dark-purple': '#4d13d1',
+      'light-gray': '#c7c3c0',
+      'dark-gray': '#4f4f4f',
+      'dark-orange': '#ff962a',
+    }
+    ,
+    extend: {},
+  },
+  plugins: [],
+}
