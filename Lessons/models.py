0fddcc77668850e2dd26864e2fa5ef570649bf81
auwal <a4auwal@gmail.com>
2022-07-17 21:50:56 +0100

    Create models for lesson

--- Lessons/models.py
+++ Lessons/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from parler.models import TranslatableModel, TranslatedFields
+from ckeditor.fields import RichTextField
+
 
 # Create your models here.
+class Lesson(TranslatableModel):
+    translations = TranslatedFields(
+        title=models.CharField(max_length=100, blank=False, null=False),
+        desc=models.TextField(max_length=400, blank=False, null=False),
+        blog=RichTextField('Blog', help_text='Edit and enter text just like MS Word.'),
+        audio=models.FileField(),
+    )
+    finished = models.BooleanField(default=False)
+    date_published = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
