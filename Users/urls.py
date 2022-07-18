a9bca8eae85756a159ba08f73c163f47a823ae93
Bukhari Auwal Idris <idrisbukhariauwal@gmail.com>
2022-07-18 12:06:06 -0700

    fixed bug

--- Users/urls.py
+++ Users/urls.py
@@ -3,9 +3,11 @@ from .views import index, singup, login, education, blog
 
 app_name = "users"
 
-urlpatterns = [path("", index, name="index"),
+urlpatterns = [
+    path("", index, name="index"),
                path("login/", login, name="login"),
                path("signup/", singup, name="signup"),
                path("blog/", blog, name="blog")
                path("education/", education, name="education"),
+]
 
