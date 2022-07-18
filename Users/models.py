66bad4e4fe549d166fda6317ac412624ae6859f9
Bukhari Auwal Idris <idrisbukhariauwal@gmail.com>
2022-07-18 15:11:30 -0700

    created custom user model

--- Users/models.py
+++ Users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from phonenumber_field.modelfields import PhoneNumberField
+
 
 # Create your models here.
+
+class User(AbstractUser):
+    name = models.CharField(max_length=100)
+    phone = PhoneNumberField()
+    is_verified = models.BooleanField(default=False)
+    is_number_verified = models.BooleanField(default=False)
