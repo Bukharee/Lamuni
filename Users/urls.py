b978f4c0d5edac43cd2bad2fd441cbaaaaf3bbd1
Bukhari Auwal Idris <idrisbukhariauwal@gmail.com>
2022-07-18 18:12:23 -0700

    configured form errors and styled in frontend

--- Users/urls.py
+++ Users/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
-from .views import index, singup, education, blog
+from .views import index, register, education, blog
+from django.contrib.auth import views as auth_views
+
 
 app_name = "users"
 
 urlpatterns = [
     path("", index, name="index"),
-               path("signup/", singup, name="signup"),
+               path("signup/", register, name="signup"),
                path("blog/", blog, name="blog"),
                path("education/", education, name="education"),
+            path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html')),
+
 ]
 
--- Users/views.py
+++ Users/views.py
@@ -1,15 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView
 from .forms import CustomUserCreationForm
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+@login_required
 def index(request):
     return render(request, "index.html")
-
-
-def singup(request):
-    return render(request, "registration/signup.html")
 
 
 def blog(request):
@@ -19,7 +17,12 @@ def education(request):
     return render(request, "education.html")
 
 
-class SignUpView(CreateView):
-    form_class = CustomUserCreationForm
-    success_url = reverse_lazy("login")
-    template_name = "registration/signup.html"
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
