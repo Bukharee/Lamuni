bf702d39e2a91696f8a4aaa7ba31f7d19b633312
bbkrmuhd <bbkrmuhdsaddiq@gmail.com>
2022-07-23 12:05:23 +0100

    update home url

--- Lamuni/urls.py
+++ Lamuni/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.i18n import i18n_patterns
 urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
     path("accounts/", include("Users.urls")),
+    path("", include("Users.urls")),
     path('', include("Lessons.urls")),
     path('i18n/', include('django.conf.urls.i18n')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
