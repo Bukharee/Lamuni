32b5e0022dc01b6efa4c7df91b917fe0b0e183df
auwal <a4auwal@gmail.com>
2022-07-25 14:18:54 +0100

    wallet ref code generator

--- Wallet/views.py
+++ Wallet/views.py
@@ -1,3 +1,5 @@
+import random
+import string
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from .models import Wallet
@@ -5,7 +7,6 @@ from .models import Wallet
 
 # Create your views here.
 def view_wallet(request):
-
     user = request.user
 
     wallet = get_object_or_404(Wallet, owner=user)
@@ -14,3 +15,9 @@ def view_wallet(request):
         'wallet': wallet,
     }
     return render(request, 'wallet.html', context)
+
+
+def wallet_ref_code_generator():
+    guess = ''.join(random.choices(string.ascii_uppercase + string.digits, k=20))
+    ref_code = 'WLT' + guess
+    return ref_code
