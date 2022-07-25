bde46917fbb7b82f4bc3d1ac5a68eaa41e969424
auwal <a4auwal@gmail.com>
2022-07-25 14:50:43 +0100

    Wallet: Transfer, Withdraw, Recharge

--- Wallet/models.py
+++ Wallet/models.py
@@ -1,7 +1,11 @@
+from datetime import timezone
+
+from django.core.exceptions import ValidationError
 from django.db import models
 from djmoney.models.fields import MoneyField
-from django.contrib.auth import get_user_model
+from Users.models import User
 from djmoney.money import Money
+from django.shortcuts import get_object_or_404
 
 OWNER_TYPE = (('FSP', 'FSP'),
               ('User', 'User'))
@@ -11,86 +15,78 @@ TRANSACTION_TYPE = (('Credit', 'Credit'), ('Debit', 'Debit'))
 
 # Create your models here.
 class Wallet(models.Model):
-    owner = models.OneToOneField(get_user_model(), related_name='wallet', on_delete=models.CASCADE)
+    owner = models.OneToOneField(User, related_name='wallet', on_delete=models.CASCADE, unique=True)
     owner_type = models.CharField(choices=OWNER_TYPE, max_length=10)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
     balance = MoneyField(max_digits=14, decimal_places=2, default=Money(0, 'NGN'), default_currency='NGN')
 
-    # @classmethod
-    # def recharge_wallet(cls, amount, receiver, description):
-    #
-    #     if amount.currency != receiver.balance.currency:
-    #         converted_amount = convert_money(amount, receiver.balance.currency)
-    #     else:
-    #         converted_amount = amount
-    #
-    #     receiver.balance += converted_amount
-    #     receiver.save()
-    #     recharge_transaction = Transaction.objects.create(
-    #         amount=amount,
-    #         receiver_wallet=receiver,
-    #         transaction_type="Credit",
-    #         date_created=timezone.now,
-    #         description=description,
-    #
-    #     )
-    #
-    #     return recharge_transaction
-    #
-    # @classmethod
-    # def withdraw_wallet(cls, amount, description, wallet_to_withdraw):
-    #
-    #     if amount.currency != wallet_to_withdraw.balance.currency:
-    #         print("warning", 'currencies mismatch')
-    #
-    #     if amount > wallet_to_withdraw.balance:
-    #         raise ValidationError("Insufficient account balance, please try amount less than your balance")
-    #
-    #     wallet_to_withdraw.balance -= amount
-    #     wallet_to_withdraw.save()
-    #
-    #     recharge_transaction = Transaction.objects.create(
-    #         amount=amount,
-    #         transaction_type="Debit",
-    #         date_created=timezone.now,
-    #         description=description,
-    #
-    #     )
-    #
-    #     return recharge_transaction
-    #
-    # @classmethod
-    # def transfer_money(cls, amount, sender, receiver, description):
-    #
-    #     sender.balance -= amount
-    #     transfer_transaction = Transaction.objects.create(
-    #         amount=amount,
-    #         sender_wallet=sender,
-    #         receiver_wallet=receiver,
-    #         transaction_type="Debit",
-    #         date_created=timezone.now,
-    #         description=description,
-    #     )
-    #
-    #     if pre_convertedMoney.currency != receiver.balance.currency:
-    #         post_convertedMoney = convert_money(pre_convertedMoney, receiver.balance.currency)
-    #     else:
-    #         post_convertedMoney = pre_convertedMoney
-    #
-    #     receiver.balance += post_convertedMoney
-    #     transfer_transaction = Transaction.objects.create(
-    #         amount=amount,
-    #         sender_wallet=sender,
-    #         receiver_wallet=receiver,
-    #         transaction_type="Credit",
-    #         date_created=timezone.now,
-    #         description=description,
-    #     )
-    #
-    #     sender.save()
-    #     receiver.save()
-    #     return transfer_transaction
+    @classmethod
+    def recharge_wallet(cls, amount, receiver, description):
+
+        receiver_wallet = get_object_or_404(Wallet, owner=receiver)
+        receiver_wallet.balance += amount
+        receiver_wallet.save()
+
+        recharge_transaction = Transaction.objects.create(
+            amount=amount,
+            receiver_wallet=receiver,
+            transaction_type="Credit",
+            date_created=timezone.now,
+            description=description,
+
+        )
+
+        return recharge_transaction
+
+    @classmethod
+    def withdraw_wallet(cls, amount, description, wallet_to_withdraw):
+
+        if amount > wallet_to_withdraw.balance:
+            raise ValidationError("Insufficient account balance, please try amount less than your balance")
+
+        wallet_to_withdraw.balance -= amount
+        wallet_to_withdraw.save()
+
+        recharge_transaction = Transaction.objects.create(
+            amount=amount,
+            transaction_type="Debit",
+            date_created=timezone.now,
+            description=description,
+
+        )
+
+        return recharge_transaction
+
+    @classmethod
+    def transfer_money(cls, amount, sender, receiver, description):
+
+        sender_wallet = get_object_or_404(Wallet, owner=sender)
+        sender_wallet -= amount
+
+        transfer_transaction = Transaction.objects.create(
+            amount=amount,
+            sender=sender,
+            receiver=receiver,
+            transaction_type="Debit",
+            date_created=timezone.now,
+            description=description,
+        )
+
+        receiver_wallet = get_object_or_404(Wallet, owner=receiver)
+        receiver_wallet.balance += amount
+        transfer_transaction = Transaction.objects.create(
+            amount=amount,
+            sender=sender,
+            receiver=receiver,
+            transaction_type="Credit",
+            date_created=timezone.now,
+            description=description,
+        )
+
+        sender_wallet.save()
+        receiver_wallet.save()
+        return transfer_transaction
 
 
 class Transaction(models.Model):
@@ -100,9 +96,9 @@ class Transaction(models.Model):
                         verbose_name='Amount')
     description = models.CharField(max_length=250, default='', help_text='write your description here',
                                    verbose_name='Description', blank=True, null=True)
-    sender = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='sender',
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sender',
                                verbose_name='Sender')
-    receiver = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='receiver',
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='receiver',
                                  verbose_name='Receiver')
     date_created = models.DateTimeField(auto_now_add=True, verbose_name='Transaction Date')
     ref = models.CharField(max_length=400, unique=True)
