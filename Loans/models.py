b941a93fa28158c8af023403bd37edf9fd83618e
auwal <a4auwal@gmail.com>
2022-07-26 17:33:23 +0100

    Add db items to gitignore

--- Loans/models.py
+++ Loans/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from multiselectfield import MultiSelectField
+from decimal import Decimal
 
 # Create your models here.
 BUSINESS_SIZE = (('MICRO', 'MICRO'), ('SMALL', 'SMALL'), ('MEDIUM', 'MEDIUM'),)
@@ -37,11 +38,11 @@ class Loan(models.Model):
     collateral = models.CharField(max_length=200, blank=True, null=True)
 
 
-RECORD_CATEGORY = (('Revenue', 'Revenue'),
-                   ('Purchase', 'Purchase'),
+RECORD_CATEGORY = (('Purchase', 'Purchase'),
                    ('Expenses', 'Expenses'),
                    ('Tax', 'Tax'),
-                   ('Income', 'Income'),)
+                   ('Income', 'Income'),
+                   ('Sales', 'Sales'),)
 
 
 class Record(models.Model):
@@ -50,9 +51,36 @@ class Record(models.Model):
     category = models.CharField(max_length=45, choices=RECORD_CATEGORY, default='', verbose_name='Category')
 
 
+class SalesRecord(models.Model):
+    item_name = models.TextField(max_length=200, blank=False, null=True)
+    quantity = models.PositiveIntegerField(default=1)
+    cost_price_per_item = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+    selling_price_per_item = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+
+
 class FinancialRecord(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.DO_NOTHING)
     profit = models.DecimalField(max_digits=12, decimal_places=2, verbose_name="Profit", default=0)
     net_profit = models.DecimalField(max_digits=12, decimal_places=2, default=0)
-    monthly_capital = models.DecimalField(max_digits=12, decimal_places=2, default=0)
-    record = models.ManyToManyField(Record, blank=True)
+    revenue = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+    records = models.ManyToManyField(Record, blank=True)
+    sales_records = models.ManyToManyField(SalesRecord, blank=True)
+
+    # @property
+    # def get_net_profit(self):
+    #     net_amount_spend = 0.00  # all record entries which are not Income
+    #     monthly_capital = self.monthly_capital
+    #     all_records = self.records.all()
+    #
+    #     for record in all_records:
+    #         if record.category != "Income":
+    #             net_amount_spend += record.amount
+    #
+    #     for record in all_records:
+    #         # add an Income to monthly profit
+    #         if record.category == "Income":
+    #             monthly_capital += record.amount
+    #
+    #     net_profit = monthly_capital - net_amount_spend
+    #
+    #     return net_profit
